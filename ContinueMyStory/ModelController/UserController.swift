8e2ac15441f2b0ad0f47c5ef8fa083642584cea4
devonfaraday <christianmcmullin@gmail.com>
2017-10-11 08:14:52 -0600

    initial branch commit

---
+++ ContinueMyStory/ModelController/UserController.swift
@@ -0,0 +1,13 @@
+//
+//  UserController.swift
+//  ContinueMyStory
+//
+//  Created by Christian McMullin on 10/11/17.
+//  Copyright © 2017 Christian McMullin. All rights reserved.
+//
+
+import Foundation
+
+class UserController {
+    
+}
