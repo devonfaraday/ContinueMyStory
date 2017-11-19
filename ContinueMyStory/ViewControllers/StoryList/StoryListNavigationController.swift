c4ba5540593f900c14686512ccbe1b4d504e067d
devonfaraday <christianmcmullin@gmail.com>
2017-11-18 17:56:35 -0700

    added a tab bar controller for navigation

---
+++ ContinueMyStory/ViewControllers/StoryList/StoryListNavigationController.swift
@@ -0,0 +1,13 @@
+//
+//  StoryListNavigationController.swift
+//  ContinueMyStory
+//
+//  Created by Christian McMullin on 11/18/17.
+//  Copyright © 2017 Christian McMullin. All rights reserved.
+//
+
+import UIKit
+
+class StoryListNavigationController: UINavigationController {
+
+}
---
+++ ContinueMyStory/ViewControllers/TabBar.swift
@@ -0,0 +1,14 @@
+//
+//  TabBar.swift
+//  ContinueMyStory
+//
+//  Created by Christian McMullin on 11/18/17.
+//  Copyright © 2017 Christian McMullin. All rights reserved.
+//
+
+import UIKit
+
+class TabBar: UITabBarController {
+
+    var currentUser: User?
+}
